461cab08f49c11a847ba10636a39f8c21739de80
Suryabhan17 <suryabhanuday99@gmail.com>
2025-10-22 00:22:41 +0530

    new file

--- add.java
+++ add.java
@@ -3,6 +3,7 @@ package surya12;
 public class add {
   public static void main(String[] args) {
     System.out.println("Surya12");
+<<<<<<< HEAD
     System.out.println("surya4545");
 
 
@@ -17,5 +18,8 @@ public class add {
 >>>>>>> 2680fc2 (new file)
 
 
+=======
+    System.out.println("surya45");
+>>>>>>> 7b550bc (change surya45)
   }
 }
