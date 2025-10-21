c7185c6a91a2a3f8f6f15358f38137c41a02e6ce
Suryabhan17 <suryabhanuday99@gmail.com>
2025-10-22 00:59:08 +0530

    nre

--- add.java
+++ add.java
@@ -3,7 +3,6 @@ package surya12;
 public class add {
   public static void main(String[] args) {
     System.out.println("Surya12");
-<<<<<<< HEAD
     System.out.println("surya4545");
 
 
@@ -11,15 +10,14 @@ public class add {
 }
  class add1 {
   public static void main(String[] args) {
-<<<<<<< HEAD
-    System.out.println("Suryabh");
-=======
-    System.out.println("Surya12");
->>>>>>> 2680fc2 (new file)
+
+    System.out.println("Suryabh45");
+
+    System.out.println("Surya1245");
+
+    
 
 
-=======
-    System.out.println("surya45");
->>>>>>> 7b550bc (change surya45)
+
   }
 }
