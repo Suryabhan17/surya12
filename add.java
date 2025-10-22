d6c40948882463f8f8d993dfe9a9b7b4e2e94e41
Suryabhan17 <suryabhanuday99@gmail.com>
2025-10-22 12:08:52 +0530

    new file

--- add.java
+++ add.java
@@ -3,11 +3,7 @@ package surya12;
 public class add {
   public static void main(String[] args) {
   
-<<<<<<< HEAD
-    System.out.println("surya454545s89");
-=======
     System.out.println("surya4545Suryabhan");
->>>>>>> 550fda9 (new rebase)
 
 
   }
@@ -15,9 +11,6 @@ public class add {
  class add1 {
   public static void main(String[] args) {
     
-<<<<<<< HEAD
-    System.out.println("Suryabh45596Suryabhan");
-=======
 
     
 
@@ -27,7 +20,6 @@ public class add {
 
 
     System.out.println("Suryabh4559645");
->>>>>>> 550fda9 (new rebase)
 
 
 
